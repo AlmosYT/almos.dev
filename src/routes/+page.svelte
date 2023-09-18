3071b7f3a8468570b519d88d6213ce268674e588
AlmosYT <62188528+AlmosYT@users.noreply.github.com>
2023-09-18 18:08:45 +0200

    commit message

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<head>
+    <title>Almos</title>
+</head>
+
+<h1>Under Construction</h1>
+
+<style>
+    h1 {
+        text-align: center;
+        margin-top: 20%;
+        font-size: 5em;
+        font-style: normal;
+        font-weight: 500;
+        font-family: 'Inter', sans-serif;
+    }
+</style>
